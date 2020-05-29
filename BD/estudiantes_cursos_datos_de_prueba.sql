a96c6b5f9530af7da13b5e1c41983d44de49d4b4
Carlos Obando Avendaña <49579027+CarlosAvendana@users.noreply.github.com>
2020-05-29 13:30:12 -0600

    segundo commit

---
+++ BD/estudiantes_cursos_datos_de_prueba.sql
@@ -0,0 +1,10 @@
+use estudiantes_curso;
+
+INSERT INTO `estudiantes_curso`.`estudiante` (`id`, `nombre`, `apellido`, `edad`) VALUES ('402370159', 'Carlos', 'Obando', '22');
+INSERT INTO `estudiantes_curso`.`estudiante` (`id`, `nombre`, `apellido`, `edad`) VALUES ('117280151', 'Felipe', 'Piedra', '21');
+INSERT INTO `estudiantes_curso`.`estudiante` (`id`, `nombre`, `apellido`, `edad`) VALUES ('543892723', 'Johanna', 'Mason', '23');
+INSERT INTO `estudiantes_curso`.`estudiante` (`id`, `nombre`, `apellido`, `edad`) VALUES ('764352345', 'Christie', 'Morales', '22');
+INSERT INTO `estudiantes_curso`.`estudiante` (`id`, `nombre`, `apellido`, `edad`) VALUES ('123423454', 'Paola ', 'Zuñiga ', '22');
+INSERT INTO `estudiantes_curso`.`estudiante` (`id`, `nombre`, `apellido`, `edad`) VALUES ('756345622', 'Heison ', 'Navarro', '21');
+
+
