7850d6a9bf8abcca41b9329c74e4a45d0f13426b
Carlos Obando Avendaña <49579027+CarlosAvendana@users.noreply.github.com>
2020-05-29 13:42:57 -0600

    datos de prueba listo

--- BD/estudiantes_cursos_datos_de_prueba.sql
+++ BD/estudiantes_cursos_datos_de_prueba.sql
@@ -7,4 +7,7 @@ INSERT INTO `estudiantes_curso`.`estudiante` (`id`, `nombre`, `apellido`, `edad`
 INSERT INTO `estudiantes_curso`.`estudiante` (`id`, `nombre`, `apellido`, `edad`) VALUES ('123423454', 'Paola ', 'Zuñiga ', '22');
 INSERT INTO `estudiantes_curso`.`estudiante` (`id`, `nombre`, `apellido`, `edad`) VALUES ('756345622', 'Heison ', 'Navarro', '21');
 
-
+INSERT INTO `estudiantes_curso`.`curso` (`idcurso`, `descripcion`, `creditos`) VALUES ('500', 'EIF 500', '3');
+INSERT INTO `estudiantes_curso`.`curso` (`idcurso`, `descripcion`, `creditos`) VALUES ('312', 'EIF 321', '2');
+INSERT INTO `estudiantes_curso`.`curso` (`idcurso`, `descripcion`, `creditos`) VALUES ('434', 'AOA 434', '5');
+INSERT INTO `estudiantes_curso`.`curso` (`idcurso`, `descripcion`, `creditos`) VALUES ('412', 'PEO 412', '6');
