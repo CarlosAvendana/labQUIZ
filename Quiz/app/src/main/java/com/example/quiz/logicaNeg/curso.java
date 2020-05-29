4fa20b3f5f4d5c204d51b275de571b743ebb2391
Carlos Obando Avendaña <49579027+CarlosAvendana@users.noreply.github.com>
2020-05-29 14:24:16 -0600

    clases del modelo

--- Quiz/app/src/main/java/com/example/quiz/logicaNeg/curso.java
+++ Quiz/app/src/main/java/com/example/quiz/logicaNeg/curso.java
@@ -1,4 +1,48 @@
 package com.example.quiz.logicaNeg;
 
 public class curso {
+
+    private int idcurso;
+    private String descripcion;
+    private int cresitos;
+
+    public curso() {
+    }
+
+    public curso(int idcurso, String descripcion, int cresitos) {
+        this.idcurso = idcurso;
+        this.descripcion = descripcion;
+        this.cresitos = cresitos;
+    }
+
+    @Override
+    public String toString() {
+        return "curso{" + "idcurso=" + idcurso + ", descripcion=" + descripcion + ", cresitos=" + cresitos + '}';
+    }
+
+    public int getIdcurso() {
+        return idcurso;
+    }
+
+    public void setIdcurso(int idcurso) {
+        this.idcurso = idcurso;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getCresitos() {
+        return cresitos;
+    }
+
+    public void setCresitos(int cresitos) {
+        this.cresitos = cresitos;
+    }
+
+
 }
